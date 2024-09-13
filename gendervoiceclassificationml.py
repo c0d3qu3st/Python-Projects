a19865f895d02a0593b61d29442976acbefc75ff
c0d3qu3st <piyush72malik@gmail.com>
2024-09-13 18:44:45 +0530

    2 new files ml

---
+++ gendervoiceclassificationml.py
@@ -0,0 +1,48 @@
+import pandas as pd
+train_df= pd.read_csv("gender-voice-train.csv")
+test_df= pd.read_csv("gender-voice-test.csv")
+
+print("The number of rows and columns in train dataframe", train_df.shape)
+print("The number of rows and columns in test dataframe", test_df.shape)
+
+res_df1=train_df.isnull()
+for i in res_df1:
+  if i==True:
+    print("The value is missing at: ",i)
+
+res_df2=test_df.isnull()
+for k in res_df2:
+  if k==True:
+    print("The value is missing at: ",k)
+
+train_df_label= train_df.iloc[:,0]
+print("The count of 'female' and 'male' classes in the test dataframe: \n",train_df_label.value_counts())
+
+test_df_label= test_df.iloc[:,0]
+print("The count of 'male' and 'female' classes in the test dataframe: \n",test_df_label.value_counts())
+
+#feature variables from the 'train_df' DataFrame.
+x_train= train_df.iloc[:,1:]
+#feature variables from the 'test_df' DataFrame.
+x_test= test_df.iloc[:,1:]
+
+#target variable from the 'train_df' DataFrame.
+y_train= train_df.iloc[:,0]
+#target variable from the 'test_df' DataFrame.
+y_test= test_df.iloc[:,0]
+
+#building a Random Forest Classifier model.
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn.metrics import confusion_matrix as c_m 
+from sklearn.metrics import classification_report as c_r
+#predicting the target variables based on the feature variables of the test DataFrame.
+rf_clf= RandomForestClassifier(n_jobs=-1, n_estimators=50)
+rf_clf.fit(x_train, y_train)
+rf_clf.score(x_train, y_train)
+y_predicted = rf_clf.predict(x_test)
+y_predicted = pd.Series(y_predicted)
+print(y_predicted.head())
+#printing the confusion matrix to see the number of TN, FN, TP and FP values.
+print(c_m(y_test, y_predicted))
+#printing the precision, recall and f1-score values for both the male and female classes.
+print(c_r(y_test, y_predicted))
---
+++ numberguessbinarymagic.py
@@ -0,0 +1,52 @@
+cards= [[],[],[],[],[],[]]
+
+def get_binary_digits(num):
+  bn_nmbs=[]
+  while num!=0:
+    quot=num//2
+    rem=num%2
+    bn_nmbs.append(rem)
+    num=quot
+  return bn_nmbs
+
+
+cards=[[] for i in range(6)]
+for n in range(1,64):
+  bn_dgts= get_binary_digits(n)
+  for i in range(len(bn_dgts)):
+      if bn_dgts[i]==1:
+        cards[i].append(n)
+
+for i in range(6):
+  print("cards",cards[i])
+
+def game():
+  player_input= input("Think of a number between 1 and 63. Type 'start' when you are ready and hit the 'Enter' key.")
+  while player_input != "start":
+    player_input= input("Think of a number between 1 and 63. Type 'start' when you are ready and hit the 'Enter' key.")
+  num=0
+  valid_entries=["yes", "no"]
+  if 2**0==1:
+    for i in cards:
+      print("card",i[0],i,": ")
+      answer=input("If the number exists in the displayed set, type 'yes' and press enter otherwise type 'no'!")
+      while answer not in valid_entries:
+        print("yes or no?")
+        answer=input("If the number exists in the displayed set, type 'yes' and press enter otherwise type 'no'!")
+      if answer=="yes":
+        num+=i[0]
+        print("Hmm...Noted!")
+      else:
+        print("Hmm...Noted!")
+  print("THE NUMBER YOU WERE THINKING OF WAS.............")
+  print("IT WAS: ",num)
+  replay= input("WANNA PLAY AGAIN??")
+  while answer not in valid_entries:
+        print("yes or no?")  
+        replay= input("WANNA PLAY AGAIN OR NO??")
+  if replay=="yes":
+    game()#recursion
+  else:
+    print("Have a good day!! Thanks for playing!!")
+    
+game()
